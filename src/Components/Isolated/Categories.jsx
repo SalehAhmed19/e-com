2d0ab0bfaaacfc9c805a3c417feeabedf1157518
Saleh Ahmed Mahin <saleh.ahmed.mahin@gmail.com>
2025-03-23 17:15:10 +0600

    category-edited

--- src/Components/Isolated/Categories.jsx
+++ src/Components/Isolated/Categories.jsx
@@ -9,11 +9,14 @@ function Categories() {
   }, []);
 
   return (
-    <div className="grid grid-cols-6 gap-5 my-20">
+    <div className="grid grid-cols-6 gap-10 my-20">
       {categories.slice(0, 6).map((category) => (
-        <div className="shadow-lg flex flex-col items-center p-5">
-          <h5 className="font-bold">{category.name}</h5>
-          <p className="text-[#848484]">{category.items} Items</p>
+        <div className="bg-gradient-to-b from-[#eedec358] to-white p-5 rounded-sm">
+          <div className="shadow-lg shadow-[#dedede44] flex flex-col items-center p-5 bg-white rounded-sm">
+            <img className="w-10 mb-2" src={category.icon} alt="" />
+            <h5 className="font-bold">{category.name}</h5>
+            <p className="text-[#848484]">{category.items} Items</p>
+          </div>
         </div>
       ))}
     </div>
